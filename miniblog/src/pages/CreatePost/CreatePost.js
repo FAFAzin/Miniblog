bb1a7c1a8adf4135117651613cf85d1a3261e5c1
JosafaSilveira <josafajosina@gmail.com>
2022-08-23 17:11:03 -0300

    adicionando interface de criar post

--- miniblog/src/pages/CreatePost/CreatePost.js
+++ miniblog/src/pages/CreatePost/CreatePost.js
@@ -1,9 +1,75 @@
 import styles from './CreatePost.module.css';
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {useAuthvalue} from '../../Context/AuthContext';
+
+
 const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [body, setBody] = useState('');
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   return (
-    <div>
-        <h1>Nova Publicação</h1>
+    <div className={styles.create_post}>
+      <h1>Crie um Post</h1>
+      <p>Escreva sobre qualquer coisa </p>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          {/*input titulo */}
+          <span>Título:</span>
+          <input type="text"
+          name='title'
+          required
+          placeholder='Pense em um bom título...'
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          />
+        </label>
+        <label>
+          {/*input imagem */}
+          <span>Url da imagem:</span>
+          <input type="text"
+          name='image'
+          required
+          placeholder='Insira sua foto...'
+          onChange={(e) => setImage(e.target.value)}
+          value={image}
+          />
+        </label>
+        <label>
+          {/*textearea descrição*/}
+          <span>Descrição:</span>
+          <textarea name="body"
+          required
+          placeholder='insira uma descrição'
+          onChange={(e) => setBody(e.target.value)}
+          value={body}
+          ></textarea>
+        </label>
+        <label>
+          {/*input tags */}
+          <span>Tags:</span>
+          <input type="text"
+          name='tags'
+          required
+          placeholder='insira tags ...'
+          onChange={(e) => setTags(e.target.value)}
+          value={tags}
+          />
+        </label>
+        {/*botão submit  */}
+        <button className='btn'>Publicar</button>
+
+        {/*{!loading && <button className='btn'>Cadastrar</button>}
+                {loading && <button className='btn' disabled>Carregando...</button>}*/}
+      </form>
     </div>
   )
 }
