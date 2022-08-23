30940b6ddfc58244cc69dc7e1be648b9214fe36c
JosafaSilveira <josafajosina@gmail.com>
2022-08-22 21:11:30 -0300

    Bloqueando páginas que precisam de autenticação

--- miniblog/src/pages/About/About.js
+++ miniblog/src/pages/About/About.js
@@ -1,10 +1,20 @@
 //CSS
 import styles from './About.module.css';
 
+import {Link} from 'react-router-dom'
+
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
+    <div className={styles.about}>
+        <h2>
+          Sobre o Mini <span>Blog</span>
+        </h2>
+        <p>Este Programa consiste em um blog feito 
+          com react no front-end e firebase no back-end
+        </p>
+        <Link to='/post/create' className='btn'>
+          Criar post
+        </Link>
     </div>
   )
 }
