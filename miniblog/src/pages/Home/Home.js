bf675e2a4fa71d432424ded973feb2d6eeae5666
JosafaSilveira <josafajosina@gmail.com>
2022-08-25 16:15:48 -0300

    adicionando component para mostrar post na home

--- miniblog/src/pages/Home/Home.js
+++ miniblog/src/pages/Home/Home.js
@@ -4,13 +4,16 @@ import styles from './Home.module.css';
 //Hooks 
 import {useNavigate, Link} from 'react-router-dom';
 import {useState} from 'react';
+import {useFetchDocuments} from '../../Hooks/useFetchDocuments';
 
 //components 
+import PostDetail from '../../components/PostDetail';
 
 const Home = () => {
 
   const [query, setQuery] = useState('');
-  const [posts] = useState([]);
+  const {documents: posts, loading} = useFetchDocuments('posts');
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,7 +27,10 @@ const Home = () => {
           <button className='btn btn-dark'> Buscar </button>
         </form>
         <div>
-          <h1>Posts...</h1>
+          {loading && <p>Carregando...</p>}
+          {posts && posts.map((post) => (
+            <PostDetail key={post.id} post={post} />
+          ))}
           {posts && posts.length === 0 && (
             <div className={styles.noposts}>
               <p>Nenhum post foi encontrado!</p>
